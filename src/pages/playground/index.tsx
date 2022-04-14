a707a777cdbc3a75514a5b21c1eb126bc8652e3a
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2022-04-14 09:46:29 +0200

    feat: Add scaffold for playground

---
+++ src/pages/playground/index.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import { Container } from "@mantine/core";
+
+import { Breadcrumbs, Page } from "~/components";
+
+export default function Playground() {
+  return (
+    <>
+      <Page>
+        <Head>
+          <title>Morten Broesby-Olsen :: Playground</title>
+        </Head>
+
+        <Container pt={20} pb={20}>
+          <Breadcrumbs
+            items={[
+              { title: "Home", href: "/" },
+              { title: "Playground", href: "/playground" },
+            ]}
+          />
+        </Container>
+      </Page>
+    </>
+  );
+}
