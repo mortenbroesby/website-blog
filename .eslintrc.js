1bcdb681c9ea571a29ef3728553756c6743f901b
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2023-05-25 23:28:26 +0200

    project(big-bang): Commit new structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,10 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true,
-    node: true,
-  },
-
+  root: true,
+  // This tells ESLint to load the config from the package `eslint-config-custom`
+  extends: ["custom"],
   settings: {
-    react: {
-      version: "detect",
+    next: {
+      rootDir: ["apps/*/"],
     },
-  },
-
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "next/core-web-vitals",
-    "next",
-    "prettier",
-  ],
-
-  parser: "@typescript-eslint/parser",
-
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-
-  plugins: ["react", "@typescript-eslint", "unused-imports", "prettier"],
-
-  rules: {
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
-    "unused-imports/no-unused-imports": "error",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "react-hooks/exhaustive-deps": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "@next/next/link-passhref": "off",
-
-    "no-duplicate-imports": "error",
   },
 };
--- src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./useRefClicked";
-export * from "./useSound";
-export * from "./useWindowSize";
