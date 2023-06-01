12693c4b5d2b6ccb305172734466b1943d2f0c11
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2023-06-02 00:54:31 +0200

    style: 💄 Let's remove description

--- changelog.config.js
+++ changelog.config.js
@@ -14,7 +14,7 @@ module.exports = {
   ],
   maxMessageLength: 64,
   minMessageLength: 3,
-  questions: ["type", "scope", "subject", "body", "lerna"],
+  questions: ["type", "scope", "subject", "lerna"],
   scopes: [],
   types: {
     chore: {
